07f4dda6674c6bf5e57d69d127b0fb39d6337ae3
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-06-22 15:32:03 +0200

    Modify slug on form delete

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -44,7 +44,7 @@
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title">Delete current</h5>
+                                    <h5 class="modal-title">Delete {{$post->slug}}</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
